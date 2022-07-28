81317cde1a91a4256dbde0c459940efa734b5745
Clara <ferreira.clara22@hotmail.com>
2022-07-27 23:01:04 -0300

    funções ok (?)

--- src/controllers/Clients.ts
+++ src/controllers/Clients.ts
@@ -12,10 +12,49 @@ class Clients {
         console.log(error)
         }
     }
-    async update(req: Request, res: Response) {}
-    async findAll(req: Request, res: Response) {}
-    async findOne(req: Request, res: Response) {}
-    async delete(req: Request, res: Response) {} 
+    
+    async findAll(req: Request, res: Response) {
+        try {
+            const response = await db.query('SELECT * FROM clients');
+            res.status(201).json(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async findOne(req: Request, res: Response) {
+        const {id} = req.params
+        try {
+            const response = await db.query('UPDATE clients SET ? WHERE id = ?', [id]);
+            res.status(201).json(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async update(req: Request, res: Response) {
+        const {id} = req.params
+        const {name, amount} = req.body
+
+        try {
+            const response = await db.query('UPDATE clients SET ? WHERE id = ?', [name, amount, id]);
+            res.json(response);
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    async delete(req: Request, res: Response) {
+        const {id} = req.params
+
+        try {
+            const response = await db.query(`DELETE FROM clients WHERE id = ${id}`);
+            res.json(response);
+        } catch (error) {
+            console.log(error);
+        }
+    } 
 }
 
 export default Clients; 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import express, {Request, Response} from 'express';
+import express, {json, Request, Response} from 'express';
 import { routerClient } from './routes/routerClient';
+import cors from 'cors'
 
 const PORT = 3000;
 
-const app = express();
+const app = express()
 
-app.use('/clients', routerClient)
+app.use(express.json())
+app.use(cors())
+
+app.use(routerClient)
 
 app.listen(PORT, () => console.log('Servidor funcionando na porta' + PORT));
